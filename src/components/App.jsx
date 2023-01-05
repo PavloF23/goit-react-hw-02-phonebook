db704330d5a9b7feda8bd386e70f9b593e98c729
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-01-05 19:36:39 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,16 @@
-export const App = () => {
+import { Component } from 'react';
+
+
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+
+  render() {
   return (
     <div
       style={{
@@ -10,7 +22,14 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
     </div>
   );
+};
 };
